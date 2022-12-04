e77606efc25b96e6912cd282d07b26f62eaa810f
Charlie Puente <puentematos@gmail.com>
2022-12-04 02:48:16 -0500

    Restructured application, removed react framework

---
+++ server.ts
@@ -0,0 +1,13 @@
+const express = require("express")
+const PORT = process.env.port || 3001
+const app = express()
+
+app.use
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server now listening on ${PORT}.`)
+})
+
