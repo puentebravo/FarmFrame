bc3e0782e7262b13107fdabebf83b5575d09fb78
Puentebravo <puentematos@gmail.com>
2023-02-07 15:14:55 -0500

    wrote getItem route for fetching drop rates

--- server/app.py
+++ server/app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import urllib.request, json
+
+
 
 
 def create_app(text_config=None):
     app = Flask(__name__)
     app.url_map.strict_slashes= False
 
-    @app.route("/getStock")
+    @app.route("/")
     def test():
-        return "<h1>This works</h1>"
+        return render_template("index.html")
+
+    @app.route("/api/getItem/<item>")
+    def getItem(item):
+
+        api_destination = "https://api.warframestat.us/drops/search/{}".format(item)
+        
+
+        page = urllib.request.Request(api_destination, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urllib.request.urlopen(page).read()
+        res_array = json.loads(response)
+
+        return res_array
+    
+        
+
+    
         
     return app
 
