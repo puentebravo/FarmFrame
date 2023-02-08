e561d45980d99cc552abb28aa57b6ff04b61182d
Puentebravo <puentematos@gmail.com>
2023-02-08 13:46:14 -0500

    Wrote get requests to warframe API, added URL encoding/sanitization for inputs

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import urllib.request, json
+import urllib.request, json, urllib.parse
 
 
 
@@ -12,10 +12,12 @@ def create_app(text_config=None):
     def test():
         return render_template("index.html")
 
-    @app.route("/api/getItem/<item>")
+    @app.route("/api/getItemDrops/<item>")
     def getItem(item):
 
-        api_destination = "https://api.warframestat.us/drops/search/{}".format(item)
+        sanitize = item.strip()
+
+        api_destination = "https://api.warframestat.us/drops/search/{}".format(urllib.parse.quote(sanitize))
         
 
         page = urllib.request.Request(api_destination, headers={'User-Agent': 'Mozilla/5.0'})
@@ -24,9 +26,45 @@ def create_app(text_config=None):
 
         return res_array
     
-        
 
+    @app.route("/api/getMod/<mod>")
+    def getMod(mod):
+
+        sanitize = mod.strip()
+
+        api_destination = "https://api.warframestat.us/mods/search/{}".format(urllib.parse.quote(sanitize))
+
+        page = urllib.request.Request(api_destination, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urllib.request.urlopen(page).read()
+        res_array = json.loads(response)
+
+        return res_array
+
+
+    @app.route("/api/getWarFrame/<frame>")
+    def getFrame(frame):
+        sanitize = frame.strip()
+
+        api_destination = "https://api.warframestat.us/warframes/search/{}".format(urllib.parse.quote(sanitize))
+
+        page = urllib.request.Request(api_destination, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urllib.request.urlopen(page).read()
+        res_array = json.loads(response)
+
+        return res_array
+
+
+    @app.route("/api/getWeapon/<weapon>")
+    def getWeapon(weapon):
+        sanitize = weapon.strip()
+
+        api_destination = "https://api.warframestat.us/weapons/{}".format(urllib.parse.quote(sanitize))
     
+        page = urllib.request.Request(api_destination, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urllib.request.urlopen(page).read()
+        res_array = json.loads(response)
+
+        return res_array
         
     return app
 
