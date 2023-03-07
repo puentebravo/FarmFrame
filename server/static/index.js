a9d6de615b33db83650210ced343618f2b2c95f8
Puentebravo <puentematos@gmail.com>
2023-03-07 18:59:54 -0500

    Scaffolded out frontend javascript

--- server/static/index.js
+++ server/static/index.js
@@ -1,1 +1,24 @@
-console.log("Connected.")
+const searchForm = document.querySelector("#searchForm");
+const optionEl = document.querySelector("#itemSelect");
+const searchEl = document.querySelector("#itemSearch");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let selectedOpt = optionEl.value;
+  let searchVal = searchEl.value;
+
+  switch (selectedOpt) {
+    case "item":
+      console.warn("Item selected");
+      break;
+    case "mod":
+      console.warn("mod selected");
+      break;
+    case "warframe":
+      console.warn("warframe selected");
+      break;
+    default:
+      console.warn("weapon selected");
+  }
+});
