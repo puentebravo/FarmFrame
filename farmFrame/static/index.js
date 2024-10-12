f43fe7edce1e269b9b44edf6dbfcb40f8e5bdf83
Puentebravo <puentematos@gmail.com>
2024-10-12 15:49:48 -0400

    Added functionality to save, render favorites

--- farmFrame/static/index.js
+++ farmFrame/static/index.js
@@ -4,9 +4,57 @@ const searchEl = document.querySelector("#itemSearch");
 const resultEl = document.querySelector("#results");
 const resHeadEl = document.querySelector("#resHeader");
 
-const displayData = (dropData) => {
+const displayFavorites = () => {
+  const favoriteSearches = JSON.parse(localStorage.getItem("favorites")) || [];
 
-  resultEl.textContent = ""
+  resultEl.textContent = "";
+  if (favoriteSearches.length > 0) {
+    resHeadEl.textContent = "Favorite Searches";
+
+    for (let i = 0; i < favoriteSearches.length; i++) {
+      let cardContainer = document.createElement("div");
+
+      cardContainer.setAttribute("class", "resultCard font-space");
+
+      resultEl.appendChild(cardContainer);
+
+      let cardNumber = document.createElement("p");
+      cardNumber.classList.add("font-heavy");
+      cardNumber.textContent = i + 1;
+
+      let favText = document.createElement("h5");
+      favText.classList.add("text-lead");
+      favText.textContent = favoriteSearches[i].value;
+
+      let deleteBtn = document.createElement("button");
+      deleteBtn.setAttribute("class", "glass-button deleteBtn");
+      deleteBtn.innerHTML = `<i class="fa-regular fa-square-minus"></i>`;
+      deleteBtn.setAttribute("data-id", favoriteSearches[i].id);
+
+      cardContainer.appendChild(cardNumber);
+      cardContainer.appendChild(favText);
+      cardContainer.appendChild(deleteBtn);
+
+      deleteBtn.addEventListener("click", function (event) {
+        let targetId = event.target.parentNode.dataset.id;
+
+        let filteredItems = favoriteSearches.filter(
+          (item) => item.id != targetId
+        );
+
+        localStorage.setItem("favorites", JSON.stringify(filteredItems));
+
+        displayFavorites();
+      });
+    }
+  } else {
+    resHeadEl.textContent =
+      "No favorites yet - favorite a search to see it here";
+  }
+};
+
+const displayData = (dropData) => {
+  resultEl.textContent = "";
   for (let i = 0; i < dropData.length; i++) {
     let element = dropData[i];
 
@@ -19,8 +67,7 @@ const displayData = (dropData) => {
     let cardChance = document.createElement("p");
     let cardSubDiv = document.createElement("div");
 
-    cardSubDiv.classList.add("subDiv")
-
+    cardSubDiv.classList.add("subDiv");
 
     cardChance.classList.add("font-heavy");
 
@@ -31,7 +78,7 @@ const displayData = (dropData) => {
 
     resultEl.appendChild(cardContainer);
     cardContainer.appendChild(cardChance);
-    cardContainer.appendChild(cardSubDiv)
+    cardContainer.appendChild(cardSubDiv);
     cardSubDiv.appendChild(cardHeader);
     cardSubDiv.appendChild(cardPlace);
   }
@@ -64,7 +111,7 @@ const getItem = async (item) => {
     moreBtn.type = "button";
     moreBtn.setAttribute("id", "moreBtn");
 
-    let expandedDisplay = data.slice(0, 15)
+    let expandedDisplay = data.slice(0, 15);
 
     resultEl.appendChild(moreBtn);
 
@@ -82,9 +129,11 @@ const getDesc = async (item) => {
   let descHeaderEl = document.querySelector("#descHeader");
   let descTextEl = document.querySelector("#descText");
   let wikiLinkEl = document.querySelector("#wikiLink");
+  let favoriteBtnEl = document.querySelector("#saveBtn");
 
   descHeaderEl.textContent = "";
   descTextEl.textContent = "";
+  favoriteBtnEl.innerHTML = `<i class="fa-regular fa-square-plus fa-sm fa-lg"></i> Save to Favorites`
 
   if (wikiLinkEl.classList.contains("show")) {
     wikiLinkEl.classList.remove("show");
@@ -93,6 +142,8 @@ const getDesc = async (item) => {
 
   descEl.classList.remove("hide");
   descEl.classList.add("show", "card");
+  favoriteBtnEl.classList.remove("hide");
+  favoriteBtnEl.classList.add("show");
 
   if (descResponse.category == "Mods") {
     descHeaderEl.textContent = descResponse.name;
@@ -124,6 +175,20 @@ const getDesc = async (item) => {
     wikiLinkEl.classList.add("show");
     wikiLinkEl.href = descResponse.wikiaUrl;
   }
+
+  favoriteBtnEl.addEventListener("click", function () {
+    const favoriteSearches = JSON.parse(localStorage.getItem("favorites")) || [];
+    let favoriteObj = {
+      id: Math.round(Math.random() * 10000000),
+      value: item,
+    };
+
+    favoriteSearches.push(favoriteObj);
+
+    localStorage.setItem("favorites", JSON.stringify(favoriteSearches));
+
+    favoriteBtnEl.textContent = "Saved!";
+  });
 };
 
 searchForm.addEventListener("submit", (e) => {
@@ -135,3 +200,5 @@ searchForm.addEventListener("submit", (e) => {
   getDesc(searchVal);
   searchEl.value = "";
 });
+
+displayFavorites();
